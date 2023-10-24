4a85a0f72080a87f87778218d1671c1ee7abb8cf
Austin Graham <austiingraham@gmail.com>
2023-10-24 03:59:33 -0400

    added timer countdown functionality

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This application is a timed coding quiz with multiple-choice questions that feat
 
 ## Credits
 - [MDN - HTMLElement: hidden property](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hidden)
+- [How to add onclick event to start timer](https://stackoverflow.com/questions/54637148/how-to-add-onclick-event-to-start-timer)
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,16 @@ document.getElementById("start-button").addEventListener(
     document.getElementById("pregame").hidden = true;
   }
 );
+
+document.getElementById("start-button").addEventListener("click", function(){
+  var timeleft = 75;
+
+  var downloadTimer = setInterval(function function1(){
+  document.getElementById("countdown").innerHTML = "Time: " + timeleft;
+  timeleft -= 1;
+  if(timeleft <= 0){
+      clearInterval(downloadTimer);
+      document.getElementById("countdown").innerHTML = "Time is up!"
+  }
+  }, 1000);
+});
