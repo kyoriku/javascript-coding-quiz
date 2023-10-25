4bb97925e8fbc3683cf3a3f50e840195de9b6b65
Austin Graham <austiingraham@gmail.com>
2023-10-25 03:30:39 -0400

    added dynamically updated HTML & CSS but only displays strings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 var pregameEl = document.getElementById("pregame");
 var timerEl = document.getElementById("timer");
 var startButtonEl = document.getElementById("start-button")
-var startGameEl = document.getElementById("quiz-game")
 
 var timer = 75;
 
 function startQuiz() {
   pregameEl.hidden = true;
-  startGameEl.hidden = false;
   timerStart();
+  showQuiz();
 }
 
 function timerStart() {
@@ -22,26 +21,20 @@ function timerStart() {
   }, 1000);
 }
 
+function showQuiz() {
+  var h1El = document.createElement("h1");
+  document.body.appendChild(h1El)
+  h1El.innerHTML = quizQuestion
+  h1El.setAttribute("style", "margin:auto; padding-top:40px; width:50%");
+}
+
+var quizQuestion = "Commonly used datatypes DO NOT include:"
+/* var quizQuestion = [
+{
+    question: "Commonly used datatypes DO NOT include:",
+    choices: ['1. string', '2. boolean', '3. alerts', '4. numbers'],
+    answer: "3. alerts"
+},
+] */
+
 startButtonEl.addEventListener("click", startQuiz)
-
-/*
-document.getElementById("start-button").addEventListener(
-  "click",
-  () => {
-    document.getElementById("pregame").hidden = true;
-    document.getElementById("quiz-game").hidden = false;
-  },
-  false,
-);
-
-document.getElementById("start-button").addEventListener("click", function(){
-  var timeleft = 75;
-  var coundownTimer = setInterval(function function1(){
-  document.getElementById("countdown").innerHTML = "Time: " + timeleft;
-  timeleft -= 1;
-  if(timeleft <= 0){
-      clearInterval(coundownTimer);
-      document.getElementById("countdown").innerHTML = "Time is up!"
-  }
-  }, 1000);
-}); */
