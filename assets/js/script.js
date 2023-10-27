f35f52b8f54ccd6646599078ef8d3fc25df80840
Austin Graham <austiingraham@gmail.com>
2023-10-27 01:33:04 -0400

    added function to display first question, created buttons for choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,25 @@
 // Variable declarations
-var pregameElement = document.getElementById('pre-game');
 var timerElement = document.getElementById('timer');
-var startButtonElement = document.getElementById('start-button')
-var quizQuestionsElement = document.getElementById('quiz-questions');
+var pregameElement = document.getElementById('pre-game');
+var startButtonElement = document.getElementById('start-button');
+var quizContentElement = document.getElementById('quiz-content');
+var questionsElement = document.getElementById('questions');
+var choicesElement = document.getElementById('choices');
 var timer = 75;
+var availableQuestions = [];
+var currentQuestion = {};
+var currentQuestionIndex = 0;
 
-// Function to start the quiz
+// Function to start quiz
 function startQuiz() {
-  hideElement(pregameElement);
-  showElement(quizQuestionsElement)
+  pregameElement.hidden = true;
+  quizContentElement.hidden = false;
+  availableQuestions = quizQuestions;
   startTimer();
-  showQuiz();
+  displayQuestion();
 }
 
-// Function to start the timer
+// Function to start timer
 function startTimer() {
   var timerInterval = setInterval(function() {
     timerElement.textContent = 'Time: ' + timer;
@@ -25,43 +31,58 @@ function startTimer() {
   }, 1000);
 }
 
-function showQuiz() {
-  var h1El = document.createElement('h1');
-  document.body.appendChild(h1El)
-  h1El.innerHTML = quizQuestion
-  h1El.setAttribute('style', 'margin:auto; padding-top:40px; width:50%');
+// Function to display question
+function displayQuestion() {
+  currentQuestion = availableQuestions[currentQuestionIndex];
+  questionsElement.innerText = currentQuestion.question;
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var button = document.createElement('button');
+    button.innerText = currentQuestion.choices[i];
+    button.setAttribute("class", "btn");
+    choicesElement.appendChild(button);
+  }
 }
 
 // Quiz questions
-var quizQuestion = [
+var quizQuestions = [
   {
     question: 'Commonly used datatypes DO NOT include:',
     choices: ['1. string', '2. boolean', '3. alerts', '4. numbers'],
     answer: '3. alerts'
   },
   {
-    question: 'The condition in an if/else statement is enclosed with ____.',
-    choices: ['1. quotes', '2. curly braces', '3. parenthesis', '4. square brackets'],
-    answer: '3. parenthesis'
+    question: 'How do you write "Hello, World!" to the console in JavaScript?',
+    choices: ['1. console.write("Hello, World!");', '2. print("Hello, World!");', '3. console.log("Hello, World!");', '4. alert("Hello, World!");'],
+    answer: '3. console.log("Hello, World!")'
   },
   {
-    question: 'Arrays in JavaScript can be used to store ____.',
+    question: 'The condition in an if/else statement is enclosed with _____',
+    choices: ['1. "quotes"', '2. {curly braces}', '3. (parenthesis)', '4. [square brackets]'],
+    answer: '3. (parenthesis)'
+  },
+  {
+    question: 'What is the correct way to comment a single line of code in JavaScript?',
+    choices: ['1. <!--Comment-->', '2. // Comment', '3. /* Comment */', '4. # Comment'],
+    answer: '2. // Comment'
+  },
+  {
+    question: 'Arrays in JavaScript can be used to store _____.',
     choices: ['1. numbers an strings', '2. booleans', '3. other arrays', '4. all of the above'],
     answer: '4. all of the above'
+  },
+  {
+    question: 'Which property can you use in order to implement event delegation?',
+    choices: ['1. event.target', '2. event.preventDefault()', '3. event.stopPropagation()', '4. event.addEventListener()'],
+    answer: '1. event.target'
   },
   {
     question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
     choices: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
     answer: '4. console.log'
   },
-  {
-    question: 'Which property can you use in order to implement event delegation?',
-    choices: ['1. event.target', '2. event.preventDefault()', '3. event.stopPropagation()', '4. event.addEventListener()'],
-    answer: '1. event.target'
-  }
 ] 
 
-// Function to hide an element
+/* // Function to hide an element
 function hideElement(element) {
   element.setAttribute('class', 'hide');
 }
@@ -69,7 +90,7 @@ function hideElement(element) {
 // Function to show an element
 function showElement(element) {
   element.removeAttribute('class', 'hide');
-}
+} */
 
 // Event listeners
 startButtonElement.addEventListener('click', startQuiz)
